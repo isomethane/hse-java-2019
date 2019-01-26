22cf637d3597fd589c0d4a91d5f642bb4bffc54f
Maria Malysheva <mn.malysheva@mail.ru>
2019-01-27 00:17:43 +0300

    Improved descriptions

--- trie/src/main/java/ru/hse/mnmalysheva/trie/Serializable.java
+++ trie/src/main/java/ru/hse/mnmalysheva/trie/Serializable.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-/** Implementing this interface allows an object to be transformed into/from byte sequence. */
+/** Implementing this interface allows an object to be converted from/into byte sequence. */
 public interface Serializable {
-    /** Transform object into byte sequence. */
+    /** Convert object into byte sequence. */
     void serialize(OutputStream out) throws IOException;
-    /** Replace object with one got from byte sequence. */
+    /** Replace object with one converted from byte sequence. */
     void deserialize(InputStream in) throws IOException;
 }

3e2c174fba667066df4280118b20ad7cc63e5e89
Maria Malysheva <mn.malysheva@mail.ru>
2019-01-26 21:39:53 +0300

    Rewrote tests for HashTable. Splited test for every method into many, added tests for nulls and keys with same hash code.

--- hashtable/src/test/java/ru/hse/mnmalysheva/hashtable/HashTableTest.java
+++ hashtable/src/test/java/ru/hse/mnmalysheva/hashtable/HashTableTest.java
@@ -1,124 +1,350 @@
 package ru.hse.mnmalysheva.hashtable;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HashTableTest {
+    private HashTable testTable;
+
+    @BeforeEach
+    void init() {
+        testTable = new HashTable();
+    }
+
+    // size tests
 
     @Test
-    void size() {
-        var t = new HashTable();
-        assertEquals(0, t.size());
-        t.put("1", "a");
-        assertEquals(1, t.size());
-        t.put("1", "b");
-        assertEquals(1, t.size());
-        t.put("2", "c");
-        assertEquals(2, t.size());
-        t.put("3", "d");
-        assertEquals(3, t.size());
-        t.put("4", "e");
-        assertEquals(4, t.size());
-        t.remove("0");
-        assertEquals(4, t.size());
-        t.remove("1");
-        assertEquals(3, t.size());
-        t.clear();
-        assertEquals(0, t.size());
-        t.put("1", "a");
-        assertEquals(1, t.size());
+    void emptyTableSize() {
+        assertEquals(0, testTable.size());
+        testTable.put("1", "a");
+        testTable.remove("1");
+        assertEquals(0, testTable.size());
+        testTable.put("2", "b");
+        testTable.clear();
+        assertEquals(0, testTable.size());
     }
 
     @Test
-    void contains() {
-        var t = new HashTable();
-        for (int i = 1; i <= 7; i++) {
-            t.put(Integer.toString(i), "...");
-        }
-        for (int i = 5; i <= 15; i++) {
-            t.put(Integer.toString(i), "!!!");
-        }
-        for (int i = 1; i <= 15; i++) {
-            assertTrue(t.contains(Integer.toString(i)));
-        }
-        for (int i = 1; i <= 10; i++) {
-            t.remove(Integer.toString(i));
-        }
-        for (int i = 1; i <= 10; i++) {
-            assertFalse(t.contains(Integer.toString(i)));
-        }
-        for (int i = 11; i <= 15; i++) {
-            assertTrue(t.contains(Integer.toString(i)));
-        }
+    void sizeIncreasesAfterAddingNewKey() {
+        testTable.put("1", "a");
+        assertEquals(1, testTable.size());
+        testTable.put("2", "b");
+        assertEquals(2, testTable.size());
+        testTable.put("3", "c");
+        assertEquals(3, testTable.size());
+        testTable.put("4", "d");
+        assertEquals(4, testTable.size());
     }
 
     @Test
-    void get() {
-        var t = new HashTable();
-        for (int i = 1; i <= 15; i++) {
-            t.put(Integer.toString(i), "...");
-        }
-        for (int i = 5; i <= 10; i++) {
-            t.put(Integer.toString(i), "value" + i);
-        }
-        assertNull(t.get("0"));
-        for (int i = 1; i < 5; i++) {
-            assertEquals("...", t.get(Integer.toString(i)));
-        }
-        for (int i = 5; i <= 10; i++) {
-            assertEquals("value" + i, t.get(Integer.toString(i)));
-        }
-        for (int i = 11; i <= 15; i++) {
-            assertEquals("...", t.get(Integer.toString(i)));
-        }
+    void sizeDoesNotIncreaseAfterAddingExistingKey() {
+        testTable.put("1", "a");
+        assertEquals(1, testTable.size());
+        testTable.put("1", "b");
+        assertEquals(1, testTable.size());
     }
 
     @Test
-    void put() {
-        var t = new HashTable();
-        for (int i = 1; i <= 15; i++) {
-            assertNull(t.put(Integer.toString(i), "value" + i));
-        }
-        for (int i = 5; i <= 10; i++) {
-            assertEquals("value" + i, t.put(Integer.toString(i), "..."));
-        }
+    void sizeIncreasesAfterAddingKeyWithSameHashCode() {
+        testTable.put("AaAa", "1");
+        assertEquals(1, testTable.size());
+        testTable.put("BBBB", "2");
+        assertEquals(2, testTable.size());
+        testTable.put("AaBB", "3");
+        assertEquals(3, testTable.size());
+        testTable.put("BBAa", "4");
+        assertEquals(4, testTable.size());
     }
 
     @Test
-    void remove() {
-        var t = new HashTable();
-        for (int i = 1; i <= 15; i++) {
-            t.put(Integer.toString(i), "value" + i);
-        }
-        assertNull(t.remove("0"));
-        for (int i = 5; i <= 10; i++) {
-            assertEquals("value" + i, t.remove(Integer.toString(i)));
-        }
-        assertEquals(9, t.size());
-        for (int i = 1; i < 5; i++) {
-            assertTrue(t.contains(Integer.toString(i)));
-        }
-        for (int i = 5; i <= 10; i++) {
-            assertFalse(t.contains(Integer.toString(i)));
-        }
-        for (int i = 11; i <= 15; i++) {
-            assertTrue(t.contains(Integer.toString(i)));
-        }
+    void sizeDecreasesAfterRemovingKey() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        assertEquals(4, testTable.size());
+        testTable.remove("2");
+        assertEquals(3, testTable.size());
+        testTable.remove("3");
+        assertEquals(2, testTable.size());
+        testTable.remove("1");
+        assertEquals(1, testTable.size());
+        testTable.remove("4");
+        assertEquals(0, testTable.size());
     }
 
     @Test
-    void clear() {
-        var t = new HashTable();
-        for (int i = 1; i <= 15; i++) {
-            t.put(Integer.toString(i), "value" + i);
-        }
-        t.clear();
-        assertEquals(0, t.size());
-        for (int i = 1; i <= 15; i++) {
-            assertFalse(t.contains(Integer.toString(i)));
-        }
-        t.put("a", "b");
-        assertEquals(1, t.size());
+    void sizeDoesNotDecreasesWhenNothingRemoved() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        assertEquals(4, testTable.size());
+        testTable.remove("5");
+        assertEquals(4, testTable.size());
+    }
+
+    // contains tests
+
+    @Test
+    void containsAdded() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        assertTrue(testTable.contains("2"));
+        assertTrue(testTable.contains("3"));
+        assertTrue(testTable.contains("1"));
+        assertTrue(testTable.contains("4"));
+    }
+
+    @Test
+    void doesNotContainRemoved() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        testTable.remove("3");
+        testTable.remove("2");
+        assertFalse(testTable.contains("2"));
+        assertFalse(testTable.contains("3"));
+        assertTrue(testTable.contains("1"));
+        assertTrue(testTable.contains("4"));
+    }
+
+    @Test
+    void doesNotContainAnythingAfterClear() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        testTable.clear();
+        assertFalse(testTable.contains("2"));
+        assertFalse(testTable.contains("3"));
+        assertFalse(testTable.contains("1"));
+        assertFalse(testTable.contains("4"));
+    }
+
+    @Test
+    void containsKeyIfKeyWithSameHashCodeWasRemoved() {
+        testTable.put("AaAa", "1");
+        testTable.put("BBBB", "2");
+        testTable.put("AaBB", "3");
+        testTable.put("BBAa", "4");
+        testTable.remove("BBBB");
+        testTable.remove("BBAa");
+        assertTrue(testTable.contains("AaAa"));
+        assertFalse(testTable.contains("BBBB"));
+        assertTrue(testTable.contains("AaBB"));
+        assertFalse(testTable.contains("BBAa"));
+    }
+
+    // get tests
+
+    @Test
+    void getNullThrows() {
+        testTable.put("1", "a");
+        assertThrows(Throwable.class, () -> testTable.get(null));
+    }
+
+    @Test
+    void getReturnsLastAdded() {
+        testTable.put("1", "a4");
+        testTable.put("2", "b4");
+        testTable.put("3", "c4");
+        testTable.put("4", "d4");
+        testTable.put("1", "a3");
+        testTable.put("2", "b3");
+        testTable.put("3", "c3");
+        testTable.put("1", "a2");
+        testTable.put("2", "b2");
+        testTable.put("1", "a1");
+        assertEquals("a1", testTable.get("1"));
+        assertEquals("b2", testTable.get("2"));
+        assertEquals("c3", testTable.get("3"));
+        assertEquals("d4", testTable.get("4"));
+    }
+
+    @Test
+    void getReturnsNullIfKeyWasNotAdded() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        assertNull(testTable.get("5"));
+    }
+
+    @Test
+    void getReturnsNullIfKeyWasRemoved() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        testTable.remove("3");
+        testTable.remove("2");
+        assertEquals("a", testTable.get("1"));
+        assertNull(testTable.get("2"));
+        assertNull(testTable.get("3"));
+        assertEquals("d", testTable.get("4"));
+    }
+
+    @Test
+    void getReturnsRightValueForKeysWithSameHashCode() {
+        testTable.put("AaAa", "1");
+        testTable.put("BBBB", "2");
+        testTable.put("AaBB", "3");
+        testTable.put("BBAa", "4");
+        assertEquals("2", testTable.get("BBBB"));
+        assertEquals("3", testTable.get("AaBB"));
+        assertEquals("1", testTable.get("AaAa"));
+        assertEquals("4", testTable.get("BBAa"));
+    }
+
+    // put tests
+
+    @Test
+    void putNullThrows() {
+        testTable.put("1", "a");
+        assertThrows(Throwable.class, () -> testTable.put(null, "123"));
+        init();
+        testTable.put("1", "a");
+        assertThrows(Throwable.class, () -> testTable.put("123", null));
+    }
+
+    @Test
+    void putReturnsRightValue() {
+        assertNull(testTable.put("1", "a4"));
+        assertNull(testTable.put("2", "b4"));
+        assertNull(testTable.put("3", "c4"));
+        assertNull(testTable.put("4", "d4"));
+        assertEquals("a4", testTable.put("1", "a3"));
+        assertEquals("b4", testTable.put("2", "b3"));
+        assertEquals("c4", testTable.put("3", "c3"));
+        assertEquals("a3", testTable.put("1", "a2"));
+        assertEquals("b3", testTable.put("2", "b2"));
+        assertEquals("a2", testTable.put("1", "a1"));
+    }
+
+    @Test
+    void putAfterReturnReturnsNull() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        testTable.remove("3");
+        testTable.remove("2");
+        assertNull(testTable.put("2", "B"));
+        assertNull(testTable.put("3", "C"));
+    }
+
+    @Test
+    void putReturnsRightValueForKeysWithSameHashCode() {
+        testTable.put("AaAa", "1");
+        testTable.put("BBBB", "2");
+        testTable.put("AaBB", "3");
+        testTable.put("BBAa", "4");
+        assertEquals("2", testTable.put("BBBB", "j"));
+        assertEquals("3", testTable.put("AaBB", "a"));
+        assertEquals("1", testTable.put("AaAa", "v"));
+        assertEquals("4", testTable.put("BBAa", "a"));
+    }
+
+    // remove tests
+
+    @Test
+    void removeNullThrows() {
+        testTable.put("1", "a");
+        assertThrows(Throwable.class, () -> testTable.remove(null));
+    }
+
+    @Test
+    void removeReturnsLastAdded() {
+        testTable.put("1", "a4");
+        testTable.put("2", "b4");
+        testTable.put("3", "c4");
+        testTable.put("4", "d4");
+        testTable.put("1", "a3");
+        testTable.put("2", "b3");
+        testTable.put("3", "c3");
+        testTable.put("1", "a2");
+        testTable.put("2", "b2");
+        testTable.put("1", "a1");
+        assertEquals("a1", testTable.remove("1"));
+        assertEquals("b2", testTable.remove("2"));
+        assertEquals("c3", testTable.remove("3"));
+        assertEquals("d4", testTable.remove("4"));
+    }
+
+    @Test
+    void removeReturnsNullIfKeyWasNotAdded() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        assertNull(testTable.remove("5"));
+    }
+
+    @Test
+    void removeReturnsNullIfKeyWasRemoved() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        testTable.remove("3");
+        testTable.remove("2");
+        assertNull(testTable.remove("2"));
+        assertNull(testTable.remove("3"));
+    }
+
+    @Test
+    void removeDoesNotAffectOtherElements() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        testTable.remove("3");
+        testTable.remove("2");
+        assertEquals("a", testTable.get("1"));
+        assertEquals("d", testTable.get("4"));
+    }
+
+    @Test
+    void removeReturnsRightValueForKeysWithSameHashCode() {
+        testTable.put("AaAa", "1");
+        testTable.put("BBBB", "2");
+        testTable.put("AaBB", "3");
+        testTable.put("BBAa", "4");
+        assertEquals("2", testTable.remove("BBBB"));
+        assertEquals("3", testTable.remove("AaBB"));
+        assertEquals("1", testTable.remove("AaAa"));
+        assertEquals("4", testTable.remove("BBAa"));
+    }
+
+    // remove tests
+
+    @Test
+    void clearRemovesAllKeys() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        testTable.clear();
+        assertNull(testTable.get("2"));
+        assertNull(testTable.get("3"));
+        assertNull(testTable.get("1"));
+        assertNull(testTable.get("4"));
+    }
+
+    @Test
+    void clearDoesNotBreakTable() {
+        testTable.put("1", "a");
+        testTable.put("2", "b");
+        testTable.put("3", "c");
+        testTable.put("4", "d");
+        testTable.clear();
+        assertDoesNotThrow(() -> testTable.put("1", "a"));
+        assertEquals("a", testTable.remove("1"));
     }
 }

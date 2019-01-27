470ae8879e9323e62ed9054e8320079fada8dc56
Maria Malysheva <mn.malysheva@mail.ru>
2019-01-27 15:12:19 +0300

    Added implementation and tests for Trie methods except serialize/deserialize.

--- trie/src/main/java/ru/hse/mnmalysheva/trie/Trie.java
+++ trie/src/main/java/ru/hse/mnmalysheva/trie/Trie.java
@@ -3,6 +3,7 @@ package ru.hse.mnmalysheva.trie;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Hashtable;
 
 /**
  * Trie class.
@@ -10,17 +11,128 @@ import java.io.OutputStream;
  * Allows to add, find and remove strings in O(length).
  */
 public class Trie implements Serializable {
+    /** Trie node class. */
+    private class Node {
+        /** Flag is true when node represents complete string. */
+        boolean isTerminal = false;
+        /** Number of terminal nodes in subtree. */
+        int size = 0;
+        /** Character-node map of children. */
+        Hashtable<Character, Node> children = new Hashtable<>();
+
+        /** Set isTerminal to new state. Recounts subtree size when state changed.
+         * @param state New terminal state.
+         * @return true if state changed, false otherwise.
+         */
+        boolean switchTerminal(boolean state) {
+            if (isTerminal == state) {
+                return false;
+            }
+            isTerminal = state;
+            size += isTerminal ? 1 : -1;
+            return true;
+        }
+
+        /** Add string to trie.
+         * @param element String to add.
+         * @param pos Next char position.
+         * @return true if trie did not contain this string, false otherwise.
+         */
+        boolean add(String element, int pos) {
+            if (pos == element.length()) {
+                return switchTerminal(true);
+            }
+            var nextChar = element.charAt(pos);
+            var child = children.get(nextChar);
+            if (child == null) {
+                child = new Node();
+                children.put(nextChar, child);
+            }
+            boolean added = child.add(element, pos + 1);
+            if (added) {
+                size++;
+            }
+            return added;
+        }
+
+        /** Check if trie contains specified string.
+         * @param pos Next char position.
+         */
+        boolean contains(String element, int pos) {
+            if (pos == element.length()) {
+                return isTerminal;
+            }
+            var nextChar = element.charAt(pos);
+            var child = children.get(nextChar);
+            if (child == null) {
+                return false;
+            }
+            return child.contains(element, pos + 1);
+        }
+
+        /** Remove string from trie.
+         * @param element String to remove.
+         * @param pos Next char position.
+         * @return true if trie contained this string, false otherwise.
+         */
+        boolean remove(String element, int pos) {
+            if (pos == element.length()) {
+                return switchTerminal(false);
+            }
+            var nextChar = element.charAt(pos);
+            var child = children.get(nextChar);
+            if (child == null) {
+                return false;
+            }
+            boolean removed = child.remove(element, pos + 1);
+            if (removed) {
+                size--;
+                if (child.size == 0) {
+                    children.remove(nextChar);
+                }
+            }
+            return removed;
+        }
+
+        /** Number of strings starting with specified prefix.
+         * @param pos Next char position.
+         */
+        int howManyStartWithPrefix(String prefix, int pos) {
+            if (pos == prefix.length()) {
+                return size;
+            }
+            var nextChar = prefix.charAt(pos);
+            var child = children.get(nextChar);
+            if (child == null) {
+                return 0;
+            }
+            return child.howManyStartWithPrefix(prefix, pos + 1);
+        }
+    }
+
+    /** Tree root represents empty string. */
+    private Node root = new Node();
+
+    /** Throw exception if string is null. */
+    void checkString(String element) {
+        if (element == null) {
+            throw new IllegalArgumentException("null strings are not allowed");
+        }
+    }
+
     /** Add string to trie.
      * @param element String to add.
      * @return true if trie did not contain this string, false otherwise.
      */
     public boolean add(String element) {
-        return false;
+        checkString(element);
+        return root.add(element, 0);
     }
 
     /** Check if trie contains specified string. */
     public boolean contains(String element) {
-        return false;
+        checkString(element);
+        return root.contains(element, 0);
     }
 
     /** Remove string from trie.
@@ -28,17 +140,19 @@ public class Trie implements Serializable {
      * @return true if trie contained this string, false otherwise.
      */
     public boolean remove(String element) {
-        return false;
+        checkString(element);
+        return root.remove(element, 0);
     }
 
     /** Number of strings in trie. */
     public int size() {
-        return 0;
+        return root.size;
     }
 
     /** Number of strings starting with specified prefix. */
     public int howManyStartWithPrefix(String prefix) {
-        return 0;
+        checkString(prefix);
+        return root.howManyStartWithPrefix(prefix, 0);
     }
 
     /** {@inheritDoc} */
---
+++ trie/src/test/java/ru/hse/mnmalysheva/trie/TrieTest.java
@@ -0,0 +1,365 @@
+package ru.hse.mnmalysheva.trie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrieTest {
+    private Trie testTrie;
+
+    @BeforeEach
+    void init() {
+        testTrie = new Trie();
+    }
+
+    // add tests
+
+    @Test
+    void addNullThrows() {
+        assertThrows(IllegalArgumentException.class, () -> testTrie.add(null));
+    }
+
+    @Test
+    void addNewStringReturnsTrue() {
+        assertTrue(testTrie.add("Test1String"));
+        assertTrue(testTrie.add("Test2String"));
+        assertTrue(testTrie.add("Test3String"));
+        assertTrue(testTrie.add("Test"));
+        assertTrue(testTrie.add("test"));
+        assertTrue(testTrie.add("String"));
+        assertTrue(testTrie.add("РусскийТекст"));
+        assertTrue(testTrie.add("РусскийТекстПодлиннее"));
+    }
+
+    @Test
+    void addExistingStringReturnsFalse() {
+        testTrie.add("Test1String");
+        testTrie.add("Test2String");
+        testTrie.add("Test3String");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        assertFalse(testTrie.add("Test1String"));
+        assertFalse(testTrie.add("Test2String"));
+        assertFalse(testTrie.add("Test3String"));
+        assertFalse(testTrie.add("Test"));
+        assertFalse(testTrie.add("test"));
+        assertFalse(testTrie.add("String"));
+        assertFalse(testTrie.add("РусскийТекст"));
+        assertFalse(testTrie.add("РусскийТекстПодлиннее"));
+    }
+
+    // contains tests
+
+    @Test
+    void containsNullThrows() {
+        assertThrows(IllegalArgumentException.class, () -> testTrie.contains(null));
+    }
+
+    @Test
+    void doesNotContainNotAdded() {
+        testTrie.add("TestString");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        assertFalse(testTrie.contains(""));
+        assertFalse(testTrie.contains("TestString..."));
+        assertFalse(testTrie.contains("Tes"));
+        assertFalse(testTrie.contains("t"));
+        assertFalse(testTrie.contains("StrinG"));
+        assertFalse(testTrie.contains("РусскийТ"));
+        assertFalse(testTrie.contains("РусскийТекстПокороче"));
+    }
+
+    @Test
+    void containsAdded() {
+        testTrie.add("TestString");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        assertTrue(testTrie.contains("TestString"));
+        assertTrue(testTrie.contains("Test"));
+        assertTrue(testTrie.contains("test"));
+        assertTrue(testTrie.contains("String"));
+        assertTrue(testTrie.contains("РусскийТекст"));
+        assertTrue(testTrie.contains("РусскийТекстПодлиннее"));
+    }
+
+    @Test
+    void containsWorksRightAfterRemove() {
+        testTrie.add("Русский");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        testTrie.remove("Русский");
+        testTrie.remove("РусскийТекстПодлиннее");
+
+        assertFalse(testTrie.contains("Русский"));
+        assertTrue(testTrie.contains("РусскийТекст"));
+        assertFalse(testTrie.contains("РусскийТекстПодлиннее"));
+    }
+
+    @Test
+    void containsCheckDifferentPathVariants() {
+        testTrie.add("Русский");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+        assertFalse(testTrie.contains(""));
+        assertTrue(testTrie.contains("Русский"));
+        assertFalse(testTrie.contains("РусскийТ"));
+        assertTrue(testTrie.contains("РусскийТекст"));
+        assertFalse(testTrie.contains("РусскийТЕКСТ"));
+        assertTrue(testTrie.contains("РусскийТекстПодлиннее"));
+    }
+
+    // remove tests
+
+    @Test
+    void removeNullThrows() {
+        assertThrows(IllegalArgumentException.class, () -> testTrie.remove(null));
+    }
+
+    @Test
+    void removeNewStringReturnsFalse() {
+        testTrie.add("TestString");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        assertFalse(testTrie.remove(""));
+        assertFalse(testTrie.remove("TestString..."));
+        assertFalse(testTrie.remove("Tes"));
+        assertFalse(testTrie.remove("t"));
+        assertFalse(testTrie.remove("StrinG"));
+        assertFalse(testTrie.remove("РусскийТ"));
+        assertFalse(testTrie.remove("РусскийТекстПокороче"));
+    }
+
+    @Test
+    void removeExistingStringReturnsTrue() {
+        testTrie.add("Test1String");
+        testTrie.add("Test2String");
+        testTrie.add("Test3String");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        assertTrue(testTrie.remove("Test1String"));
+        assertTrue(testTrie.remove("Test2String"));
+        assertTrue(testTrie.remove("Test3String"));
+        assertTrue(testTrie.remove("Test"));
+        assertTrue(testTrie.remove("test"));
+        assertTrue(testTrie.remove("String"));
+        assertTrue(testTrie.remove("РусскийТекст"));
+        assertTrue(testTrie.remove("РусскийТекстПодлиннее"));
+    }
+
+    @Test
+    void removeDoesNotAffectOtherStrings() {
+        testTrie.add("Test1String");
+        testTrie.add("Test2String");
+        testTrie.add("Test3String");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        testTrie.remove("Test1String");
+        testTrie.remove("Test2String");
+        testTrie.remove("Test");
+        testTrie.remove("Русский");
+        testTrie.remove("РусскийТекстПодлиннее");
+
+        assertTrue(testTrie.contains("Test3String"));
+        assertTrue(testTrie.contains("test"));
+        assertTrue(testTrie.contains("String"));
+        assertTrue(testTrie.contains("РусскийТекст"));
+    }
+
+    @Test
+    void doesNotContainRemovedStrings() {
+        testTrie.add("Test1String");
+        testTrie.add("Test2String");
+        testTrie.add("Test3String");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        testTrie.remove("Test1String");
+        testTrie.remove("Test2String");
+        testTrie.remove("Test");
+        testTrie.remove("Русский");
+        testTrie.remove("РусскийТекстПодлиннее");
+
+        assertFalse(testTrie.contains("Test1String"));
+        assertFalse(testTrie.contains("Test2String"));
+        assertFalse(testTrie.contains("Test"));
+        assertFalse(testTrie.contains("Русский"));
+        assertFalse(testTrie.contains("РусскийТекстПодлиннее"));
+    }
+
+    // size tests
+
+    @Test
+    void emptyTrieSize() {
+        assertEquals(0, testTrie.size());
+    }
+
+    @Test
+    void sizeChangesCorrectlyAfterAdd() {
+        testTrie.add("Test1String");
+        assertEquals(1, testTrie.size());
+        testTrie.add("Test2String");
+        assertEquals(2, testTrie.size());
+        testTrie.add("Test3String");
+        assertEquals(3, testTrie.size());
+        testTrie.add("Test");
+        assertEquals(4, testTrie.size());
+        testTrie.add("test");
+        assertEquals(5, testTrie.size());
+        testTrie.add("String");
+        assertEquals(6, testTrie.size());
+        testTrie.add("Test2String");
+        assertEquals(6, testTrie.size());
+        testTrie.add("Test");
+        assertEquals(6, testTrie.size());
+        testTrie.add("РусскийТекст");
+        assertEquals(7, testTrie.size());
+        testTrie.add("String");
+        assertEquals(7, testTrie.size());
+        testTrie.add("РусскийТекстПодлиннее");
+        assertEquals(8, testTrie.size());
+    }
+
+    @Test
+    void sizeChangesCorrectlyAfterRemove() {
+        testTrie.add("Test1String");
+        testTrie.add("Test2String");
+        testTrie.add("Test3String");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        testTrie.remove("Test1String");
+        assertEquals(7, testTrie.size());
+        testTrie.remove("Test2String");
+        assertEquals(6, testTrie.size());
+        testTrie.remove("Test3String...");
+        assertEquals(6, testTrie.size());
+        testTrie.remove("Test");
+        assertEquals(5, testTrie.size());
+        testTrie.remove("Русский");
+        assertEquals(5, testTrie.size());
+        testTrie.remove("РусскийТекстПодлиннее");
+        assertEquals(4, testTrie.size());
+    }
+
+    // howManyStartWithPrefix tests
+
+    @Test
+    void sizeEqualsHowManyStartWithEmptyPrefix() {
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("Test1String");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("Test2String");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("Test3String");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("Test");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("test");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("String");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("Test2String");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("Test");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("РусскийТекст");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("String");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.add("РусскийТекстПодлиннее");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+
+        testTrie.remove("Test1String");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.remove("Test2String");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.remove("Test3String...");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.remove("Test");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.remove("Русский");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+        testTrie.remove("РусскийТекстПодлиннее");
+        assertEquals(testTrie.size(), testTrie.howManyStartWithPrefix(""));
+    }
+
+    @Test
+    void howManyStartWithNotExistingPrefix() {
+        testTrie.add("Test1String");
+        testTrie.add("Test2String");
+        testTrie.add("Test3String");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        assertEquals(0, testTrie.howManyStartWithPrefix("А"));
+        assertEquals(0, testTrie.howManyStartWithPrefix("Test0"));
+        assertEquals(0, testTrie.howManyStartWithPrefix("StrinG"));
+        assertEquals(0, testTrie.howManyStartWithPrefix("String2"));
+        assertEquals(0, testTrie.howManyStartWithPrefix("РусскийТекст1"));
+    }
+
+    @Test
+    void howManyStartWithExistingPrefix() {
+        testTrie.add("Test1String");
+        testTrie.add("Test2String");
+        testTrie.add("Test3String");
+        testTrie.add("Test");
+        testTrie.add("test");
+        testTrie.add("String");
+        testTrie.add("Русский");
+        testTrie.add("РусскийТекст");
+        testTrie.add("РусскийТекстПодлиннее");
+
+        assertEquals(4, testTrie.howManyStartWithPrefix("Test"));
+        assertEquals(1, testTrie.howManyStartWithPrefix("Test1"));
+        assertEquals(1, testTrie.howManyStartWithPrefix("S"));
+        assertEquals(3, testTrie.howManyStartWithPrefix("Рус"));
+        assertEquals(3, testTrie.howManyStartWithPrefix("Русский"));
+        assertEquals(2, testTrie.howManyStartWithPrefix("РусскийТ"));
+        assertEquals(2, testTrie.howManyStartWithPrefix("РусскийТекст"));
+        assertEquals(1, testTrie.howManyStartWithPrefix("РусскийТекстПодлиннее"));
+    }
+
+    @Test
+    void serialize() {
+    }
+
+    @Test
+    void deserialize() {
+    }
+}

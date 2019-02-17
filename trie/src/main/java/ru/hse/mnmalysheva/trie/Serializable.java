084af11c0fd3df043c78ea8f9d2ebd158c3d9729
Maria Malysheva <mn.malysheva@mail.ru>
2019-02-17 18:06:59 +0300

    Added @NotNull annotations to Serializable interface

--- trie/src/main/java/ru/hse/mnmalysheva/trie/Serializable.java
+++ trie/src/main/java/ru/hse/mnmalysheva/trie/Serializable.java
@@ -1,5 +1,7 @@
 package ru.hse.mnmalysheva.trie;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,7 +9,7 @@ import java.io.OutputStream;
 /** Implementing this interface allows an object to be converted from/into byte sequence. */
 public interface Serializable {
     /** Convert object into byte sequence. */
-    void serialize(OutputStream out) throws IOException;
+    void serialize(@NotNull OutputStream out) throws IOException;
     /** Replace object with one converted from byte sequence. */
-    void deserialize(InputStream in) throws IOException;
+    void deserialize(@NotNull InputStream in) throws IOException;
 }

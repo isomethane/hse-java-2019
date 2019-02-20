24465fdbeaa853bb978c6cd348b2a373f38d9e02
Maria Malysheva <mn.malysheva@mail.ru>
2019-02-20 17:24:20 +0300

    Fix

--- lab5/src/main/java/ru/hse/inclass/lab5/Serialization.java
+++ lab5/src/main/java/ru/hse/inclass/lab5/Serialization.java
@@ -9,7 +9,9 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Serialization {
-    private static  <T> void deserialize(ObjectInputStream in, Class<T> c, T instance)
+    public static void serialize(Object o, OutputStream out) {}
+
+    private static  <T> void deserialize(DataInputStream in, Class<T> c, T instance)
             throws IOException, ClassNotFoundException, IllegalAccessException {
         if (c == null) {
             return;
@@ -18,7 +20,25 @@ public class Serialization {
         var fields = c.getDeclaredFields();
         Arrays.sort(fields, Comparator.comparing(Field::getName));
         for (var f : fields) {
-            f.set(instance, in.readObject());
+            if (f.getType().equals(byte.class)) {
+                f.setByte(instance, in.readByte());
+            } else if (f.getType().equals(short.class)) {
+                f.setShort(instance, in.readShort());
+            } else if (f.getType().equals(int.class)) {
+                f.setInt(instance, in.readInt());
+            } else if (f.getType().equals(long.class)) {
+                f.setLong(instance, in.readLong());
+            } else if (f.getType().equals(float.class)) {
+                f.setFloat(instance, in.readFloat());
+            } else if (f.getType().equals(double.class)) {
+                f.setDouble(instance, in.readDouble());
+            } else if (f.getType().equals(boolean.class)) {
+                f.setBoolean(instance, in.readBoolean());
+            } else if (f.getType().equals(char.class)) {
+                f.setChar(instance, in.readChar());
+            } else if (f.getType().equals(String.class)) {
+                f.set(instance, in.readUTF());
+            }
         }
     }
 
@@ -26,7 +46,7 @@ public class Serialization {
             throws NoSuchMethodException, IllegalAccessException,
             InvocationTargetException, InstantiationException, IOException, ClassNotFoundException {
         T instance = c.getDeclaredConstructor().newInstance();
-        deserialize(new ObjectInputStream(in), c, instance);
+        deserialize(new DataInputStream(in), c, instance);
         return instance;
     }
 }
--- lab5/src/test/java/ru/hse/inclass/lab5/SerializationTest.java
+++ lab5/src/test/java/ru/hse/inclass/lab5/SerializationTest.java
@@ -2,7 +2,41 @@ package ru.hse.inclass.lab5;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SerializationTest {
+
+    @Test
+    void deserialize() throws NoSuchMethodException, IllegalAccessException, InstantiationException, ClassNotFoundException, InvocationTargetException, IOException {
+        class TestClass {
+            private int testValue = 30;
+            private String testString = "aaa";
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                TestClass testClass = (TestClass) o;
+                return testValue == testClass.testValue &&
+                        Objects.equals(testString, testClass.testString);
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(testValue, testString);
+            }
+        }
+
+        var testClass = new TestClass();
+        var out = new ByteArrayOutputStream();
+        Serialization.serialize(testClass, out);
+        var another = Serialization.deserialize(new ByteArrayInputStream(out.toByteArray()), TestClass.class);
+        assertEquals(testClass, another);
+    }
 }

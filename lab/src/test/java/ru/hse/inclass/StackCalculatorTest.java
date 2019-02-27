8daa964e3726ee75cb8619b2842beea453f9f0b3
Maria Malysheva <mn.malysheva@mail.ru>
2019-02-27 17:30:33 +0300

    Fixed test

--- lab/src/test/java/ru/hse/inclass/StackCalculatorTest.java
+++ lab/src/test/java/ru/hse/inclass/StackCalculatorTest.java
@@ -2,6 +2,7 @@ package ru.hse.inclass;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,9 +13,12 @@ class StackCalculatorTest {
     @Test
     void testAdd() {
         @SuppressWarnings("unchecked")
-        var mockList = (List<Integer>) mock(List.class);
+        var mockList = (List<Integer>) mock(ArrayList.class);
         var calculator = new StackCalculator(mockList);
-        when(mockList.get(anyInt())).thenReturn(2, 1, 5, 3);
+        when(mockList.get(anyInt())).thenReturn(2, 1, 5, 3, 8);
+        when(mockList.size()).thenReturn(1);
+
+        assertEquals(8, calculator.calculate("1 2 + 5 +"));
 
         var inOrder = inOrder(mockList);
         inOrder.verify(mockList).add(1);
@@ -28,11 +32,31 @@ class StackCalculatorTest {
         inOrder.verify(mockList).get(0);
         inOrder.verify(mockList).get(1);
         inOrder.verify(mockList).get(0);
-
-        assertEquals(6, calculator.calculate("1 2 + 5 +"));
+        inOrder.verify(mockList).get(0);
     }
 
     @Test
-    void testSubtract() {
+    void testComplex() {
+        @SuppressWarnings("unchecked")
+        var mockList = (List<Integer>) mock(ArrayList.class);
+        var calculator = new StackCalculator(mockList);
+        when(mockList.get(anyInt())).thenReturn(2, 1, 5, 3, 8);
+        when(mockList.size()).thenReturn(2, 2, 1, 1, 2, 2, 1, 1, 1);
+
+        assertEquals(8, calculator.calculate("1 2 + 5 +"));
+
+        var inOrder = inOrder(mockList);
+        inOrder.verify(mockList).add(1);
+        inOrder.verify(mockList).add(2);
+        inOrder.verify(mockList).add(3);
+        inOrder.verify(mockList).add(5);
+        inOrder.verify(mockList).add(8);
+
+        inOrder = inOrder(mockList);
+        inOrder.verify(mockList).get(1);
+        inOrder.verify(mockList).get(0);
+        inOrder.verify(mockList).get(1);
+        inOrder.verify(mockList).get(0);
+        inOrder.verify(mockList).get(0);
     }
 }

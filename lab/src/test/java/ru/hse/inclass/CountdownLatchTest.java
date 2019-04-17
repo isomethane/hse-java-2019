aa8fed4c42eb70e8eb0c970c5851ba22ba1da2a8
Serg Kuzmin <serg.kuzmin00@mail.ru>
2019-04-17 16:52:55 +0300

    add test

--- lab/src/test/java/ru/hse/inclass/CountdownLatchTest.java
+++ lab/src/test/java/ru/hse/inclass/CountdownLatchTest.java
@@ -1,7 +1,37 @@
 package ru.hse.inclass;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class CountdownLatchTest {
+    class Worker implements Runnable {
+        private final CountdownLatch start;
+        private final CountdownLatch done;
+        private final int number;
+
+        Worker(CountdownLatch start, CountdownLatch done, int number) {
+            this.start = start;
+            this.done = done;
+            this.number = number;
+        }
+
+        @Override
+        public void run() {
+            try {
+                start.await();
+                System.out.println("Worker " + number + " is working");
+                done.countDown();
+            } catch (InterruptedException ignored) {}
+        }
+    }
+
+    class Doer {
+
+    }
+
+    @Test
+    void testAwait() {
+
+    }
+
 
 }

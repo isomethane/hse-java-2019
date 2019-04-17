b74a74dba1599a9b5360bcf58d3a2a836e5ce3d3
Maria Malysheva <mn.malysheva@mail.ru>
2019-04-17 16:39:01 +0300

    interface

--- lab/src/main/java/ru/hse/inclass/CountdownLatch.java
+++ lab/src/main/java/ru/hse/inclass/CountdownLatch.java
@@ -1,4 +1,30 @@
 package ru.hse.inclass;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class CountdownLatch {
+    private final Lock lock = new ReentrantLock();
+    private final Condition waitCondition = lock.newCondition();
+    private final int maximumCount;
+    private int currentCount;
+
+    public CountdownLatch(int count) {
+        maximumCount = count;
+    }
+
+    public void await() {
+        lock.lock();
+        if (currentCount > 0) {
+            try {
+                waitCondition.await();
+            } catch (InterruptedException ignored) {}
+        }
+        lock.unlock();
+    }
+
+    public void countDown() {}
+
+    public void countUp() {}
 }

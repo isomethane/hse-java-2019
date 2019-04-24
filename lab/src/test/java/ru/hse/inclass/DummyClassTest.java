896f070048c7decfe00cd343f46e7a5a3db565dc
Maria Malysheva <mn.malysheva@mail.ru>
2019-04-24 15:59:57 +0300

    init

--- lab/src/test/java/ru/hse/inclass/DummyClassTest.java
+++ lab/src/test/java/ru/hse/inclass/DummyClassTest.java
@@ -1,7 +1,11 @@
 package ru.hse.inclass;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DummyClassTest {
-
+    @Test
+    void kek() {
+    }
 }

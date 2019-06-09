708873e4285b149fc91cad3d553f5cb1409911f2
Maria Malysheva <mn.malysheva@mail.ru>
2019-06-09 09:32:07 +0300

    FileDescription javadoc

--- ftpgui/src/main/java/ru/hse/mnmalysheva/ftpgui/FileDescription.java
+++ ftpgui/src/main/java/ru/hse/mnmalysheva/ftpgui/FileDescription.java
@@ -2,8 +2,11 @@ package ru.hse.mnmalysheva.ftpgui;
 
 import org.jetbrains.annotations.NotNull;
 
+/** This class represents simple description of file. **/
 public class FileDescription {
+    /** File name. **/
     public final String name;
+    /** Directory flag. **/
     public final boolean isDirectory;
 
     public FileDescription(@NotNull String name, boolean isDirectory) {

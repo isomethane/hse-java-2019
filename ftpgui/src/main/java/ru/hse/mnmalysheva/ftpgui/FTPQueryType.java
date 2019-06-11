b5cbad802998c446ee33064b61f54a57702e6617
Maria Malysheva <mn.malysheva@mail.ru>
2019-06-11 23:02:03 +0300

    FTPQuery javadoc

--- ftpgui/src/main/java/ru/hse/mnmalysheva/ftpgui/FTPQueryType.java
+++ ftpgui/src/main/java/ru/hse/mnmalysheva/ftpgui/FTPQueryType.java
@@ -1,25 +1,34 @@
 package ru.hse.mnmalysheva.ftpgui;
 
-public enum FTPQueryType {
-    LIST(1), GET(2);
+/** This class represents FTP query type. **/
+enum FTPQueryType {
+    LIST(FTPQueryType.LIST_CODE), GET(FTPQueryType.GET_CODE);
 
+    private static final int LIST_CODE = 1;
+    private static final int GET_CODE = 2;
     private final int code;
 
     FTPQueryType(int code) {
         this.code = code;
     }
 
+    /** Returns query code. **/
     int getCode() {
         return code;
     }
 
+    /**
+     * Converts query code to FTPQueryType.
+     * @throws IllegalArgumentException if code is not supported.
+     **/
     static FTPQueryType fromCode(int code) {
-        if (code == LIST.code) {
-            return LIST;
+        switch (code) {
+            case LIST_CODE:
+                return LIST;
+            case GET_CODE:
+                return GET;
+            default:
+                throw new IllegalArgumentException("Code is not supported");
         }
-        if (code == GET.code) {
-            return GET;
-        }
-        throw new IllegalArgumentException("Code is not supported");
     }
 }
